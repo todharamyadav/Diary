2765bf175ac280cf667685b9abc91d41bf5a1a92
Dharamvir <dharamvir@dharamvirs-air.home>
2016-08-22 12:15:30 -0400

    Initial Commit

---
+++ Diary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by Dharamvir on 8/22/16.
+//  Copyright © 2016 Dharamvir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
